e4b1575f4f7ba2b693bccc38f898c561472e04be
deepak <deepak7@gmail.com>
2017-04-08 19:58:51 -0700

    Remove unused code

--- DescriptionEnum.cs
+++ DescriptionEnum.cs
@@ -1,5 +1,4 @@
 ﻿using NullGuard;
-using System;
 using System.ComponentModel;
 using System.Reflection;
 
@@ -22,49 +21,6 @@ namespace Hspi
                 return attributes[0].Description;
             else
                 return value.ToString();
-        }
-
-        /// <summary>
-        /// Converts the string representation of the name or numeric value of one or more enumerated constants to an equivilant enumerated object.
-        /// Note: Utilised the DescriptionAttribute for values that use it.
-        /// </summary>
-        /// <param name="enumType">The System.Type of the enumeration.</param>
-        /// <param name="value">A string containing the name or value to convert.</param>
-        /// <returns></returns>
-        public static object Parse(Type enumType, string value)
-        {
-            return Parse(enumType, value, false);
-        }
-
-        /// <summary>
-        /// Converts the string representation of the name or numeric value of one or more enumerated constants to an equivilant enumerated object.
-        /// A parameter specified whether the operation is case-sensitive.
-        /// Note: Utilised the DescriptionAttribute for values that use it.
-        /// </summary>
-        /// <param name="enumType">The System.Type of the enumeration.</param>
-        /// <param name="value">A string containing the name or value to convert.</param>
-        /// <param name="ignoreCase">Whether the operation is case-sensitive or not.</param>
-        /// <returns></returns>
-        public static object Parse(Type enumType, string value, bool ignoreCase)
-        {
-            if (ignoreCase)
-            {
-                value = value.ToLowerInvariant();
-            }
-
-            foreach (System.Enum val in System.Enum.GetValues(enumType))
-            {
-                string comparisson = GetDescription(val);
-                if (ignoreCase)
-                {
-                    comparisson = comparisson.ToLowerInvariant();
-                }
-                if (GetDescription(val) == value)
-                {
-                    return val;
-                }
-            }
-            return System.Enum.Parse(enumType, value, ignoreCase);
         }
     }
 }
